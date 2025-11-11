50555a10c057d9afbbe1c4975ec3e8798ef8676f
iofahmawi <mo.fahmawi@gmail.com>
2025-11-11 13:19:17 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'prayer-times-cache-v9'; // <-- 1. تم تغيير رقم الإصدار
+const CACHE_NAME = 'prayer-times-cache-v10'; // <-- 1. تم تغيير رقم الإصدار
 const urlsToCache = [
   '/',
   'index.html',
@@ -12,7 +12,11 @@ const urlsToCache = [
   'https://iofahmawi.github.io/salat/mafraq-2025.csv',
   'https://iofahmawi.github.io/salat/ajloun-2025.csv',
   'https://iofahmawi.github.io/salat/jerash-2025.csv',
-  'https://iofahmawi.github.io/salat/karak-2025.csv'
+  'https://iofahmawi.github.io/salat/karak-2025.csv',
+  // --- 3. الملفات الجديدة التي تمت إضافتها ---
+  'https://iofahmawi.github.io/salat/tafilah-2025.csv',
+  'https://iofahmawi.github.io/salat/ma\'an-2025.csv',
+  'https://iofahmawi.github.io/salat/aqaba-2025.csv'
 ];
 
 self.addEventListener('install', event => {
